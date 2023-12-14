64c78b2f2cbd99a921b6628a5d365be356d442e2
Guilherme Sergio dos Santos <guilhermeguiglas@gmail.com>
2023-12-14 12:53:15 -0300

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,36 @@
+// Create web server
+
+// Imports
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+// Init
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+
+// Routes
+app.get('/posts/:id/comments', (req, res) => {
+    res.send([
+        {
+            id: 1,
+            content: 'Comment 1'
+        },
+        {
+            id: 2,
+            content: 'Comment 2'
+        },
+        {
+            id: 3,
+            content: 'Comment 3'
+        }
+    ]);
+});
+
+// Start server
+app.listen(4001, () => {
+    console.log('Listening on 4001');
+});
